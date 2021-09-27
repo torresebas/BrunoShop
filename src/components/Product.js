4e8fce26ea5ddd7722bbc09335b08f23e9fd1920
sebastianto <sebastt.93@gmail.com>
2021-09-27 13:50:39 +1000

    Adding Button (add to Cart)

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,30 +1,31 @@
 import { Component } from "react";
+import Button from './Button'
 
-const styles ={
+const styles = {
   product: {
-    border : 'solid 1px #eee',
-    boxShadow: '0 5px 5px rgba(0,0,0,0.1)',
+    border: "solid 1px #eee",
+    boxShadow: "0 5px 5px rgba(0,0,0,0.1)",
     width: "30%",
-    padding : '10px 15px',
-    borderRadius: '5px'
+    padding: "10px 15px",
+    borderRadius: "5px",
   },
   img: {
-    width: '100%'
-  }
-}
+    width: "100%",
+  },
+};
 
 class Product extends Component {
-  render(){
-    console.log(this.props)
-    const {product} = this.props
-    return(
+  render() {
+    const { product, addToCart } = this.props;
+    return (
       <div style={styles.product}>
         <img style={styles.img} src={product.img} alt={product.name} />
         <h3>{product.name}</h3>
         <p>{product.price}</p>
+        <Button onClick={() => addToCart(product)}>Add to cart</Button>
       </div>
-    )
+    );
   }
 }
 
-export default Product
+export default Product;
