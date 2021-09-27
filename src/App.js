6e1653e0b601a528f4f5d0fdd4b09e10d3220741
sebastianto <sebastt.93@gmail.com>
2021-09-27 12:41:08 +1000

    Initial State and images

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    products :[
+      {name:'Tomato', price: 2.50, img:'/products/tomato.jpg'},
+      {name:'Peas', price: 2.50, img:'/products/peas.jpg'},
+      {name:'Lettuce', price: 2.50, img:'/products/lettuce.jpg'}
+    ]
+  }
   render(){
     return(
       'hola'
